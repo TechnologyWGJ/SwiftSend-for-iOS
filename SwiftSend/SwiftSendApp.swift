1baef33019ee076540daa63d0a92034cba884cde
Dump <stormtech.dump@gmail.com>
2023-08-13 15:04:30 +0800

    Initial Commit

---
+++ SwiftSend/SwiftSendApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSendApp.swift
+//  SwiftSend
+//
+//  Created by Dump on 2023/8/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
